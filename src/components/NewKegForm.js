656950a23d09c3471e13602dc442de80e5c876cd
Olha Hizhytska <olgainfotech@gmail.com>
2021-04-23 14:15:44 -0700

    update newkegform component

--- src/components/NewKegForm.js
+++ src/components/NewKegForm.js
@@ -1,11 +1,54 @@
 import React from "react";
+import { v4 } from 'uuid';
+import PropTypes from "prop-types";
 
 function NewKegForm(props){
+
+  function handleNewKegFormSubmission(event) {
+    event.preventDefault();
+    props.onNewKegCreation({
+      name: event.target.name.value, 
+      brand: event.target.brand.value, 
+      price: parseInt(event.target.price.value), 
+      abv: event.target.abv.value,
+      pintsLeft: parseInt(event.target.pintsLeft.value),
+      id: v4()
+    });
+  }
+
   return (
     <React.Fragment>
-      <h3>This is a form.</h3>
+      <h3>Add a new keg:</h3>
+      <form onSubmit={handleNewKegFormSubmission}>
+        <input
+          type='text'
+          name='name'
+          placeholder='Name' />
+        <input
+          type='text'
+          name='brand'
+          placeholder='Brand' />
+        <input
+          type='number'
+          name='price'
+          placeholder='Price' />
+        <input
+          type='text'
+          name='abv'
+          placeholder='Alcohol Content' />
+        <input
+          type='number'
+          name='pintsLeft'
+          placeholder='Quantity' />
+        
+        <button type='submit'>Add</button>
+      </form>
     </React.Fragment>
   );
 }
+
+NewKegForm.propTypes = {
+  onNewTicketCreation: PropTypes.func
+};
 
 export default NewKegForm;
